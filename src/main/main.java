efd6e7f67e9e4346c47463fc855dc17b93aa92ef
DELL <singh.chirag.rana@gmail.com>
2020-04-10 07:30:25 -0400

    Initial commit

---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.*;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> information=new ArrayList<>();
+
+    }
+
+}
