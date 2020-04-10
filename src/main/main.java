b175c2621c42a3dcf14c0b464cf2e48eab194f04
DELL <singh.chirag.rana@gmail.com>
2020-04-10 07:38:13 -0400

    made an arraylist(information) which stores the information of coffemachine.add initial quantities of  water,milk,coffeebeans,disposal cups,money.

--- src/main/main.java
+++ src/main/main.java
@@ -1,11 +1,17 @@
 package main;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        ArrayList<Integer> information=new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> information = new ArrayList<>();
+        information.add(1200);//initial quantity of water
+        information.add(540);//initial quantity of milk
+        information.add(120);//initial quantity of coffeebean
+        information.add(9);//initial quantity of disposal cups
+        information.add(550);//initial ammount
 
     }
 
